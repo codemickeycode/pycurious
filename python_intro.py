31af9e3187a24c74677b965eeb99cc2d97162fbc
Micaela Reyes <codemickeycode@gmail.com>
2019-10-13 21:00:12 +0800

    Add sample notebooks

---
+++ python_intro.py
@@ -0,0 +1,44 @@
+# Hello World
+# print('Hello, The Coding School!')
+
+# ----------
+
+# our first function
+# def hi():
+#     print('Hi there!')
+#     print('How are you?')
+
+# hi()
+
+# ----------
+
+# pass a parameter
+# def hi(name):
+#     name = 'Sakura'
+#     if name == 'Sakura':
+#         print('Hey Sakura!')
+#     elif name == 'Hinata':
+#         print('Hey Hinata!')
+#     else:
+#         print('Hey anonymous!')
+
+# hi("Sakura")
+
+# ----------
+
+# parameterize name / make it dynamic
+# def hi(name):
+#     print('Hi ' + name + '!')
+
+# hi("Cheryl")
+
+# ----------
+
+# Say hi to a list of names
+# def hi(name):
+#     print('Hi ' + name + '!')
+
+# girls = ['Mickey', 'Anj', 'Alysson', 'Irene', 'Vanessa']
+# for name in girls:
+#     hi(name)
+#     print('Next girl')
